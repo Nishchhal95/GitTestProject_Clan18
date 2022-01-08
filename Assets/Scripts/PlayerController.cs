365b2d8fcddaec50a2f5994ecf885e49007e62bb
Nishchhal <nishchhalbakshi@gmail.com>
2022-01-08 21:33:14 +0530

    Added a comment on PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Comment from Github.com HELLO
     }
 
     // Update is called once per frame
